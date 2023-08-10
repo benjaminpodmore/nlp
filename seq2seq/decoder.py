54e74fcb43bda670534c0fa84f7aee552ff6611d
Benjamin Podmore <benjaminpodmore@gmail.com>
2023-08-10 23:30:51 +0800

    seq2seq with attention (ready for testing)

--- seq2seq/decoder.py
+++ seq2seq/decoder.py
@@ -1,20 +1,21 @@
 import torch
 import torch.nn as nn
-from attention import DotProductAttention
+from attention import Attention
 
 
 class Decoder(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, output_size):
+    def __init__(self, vocab_size, embedding_dim, hidden_size, output_size):
         super().__init__()
 
-        self.attention = DotProductAttention(hidden_size)
-        self.rnn = nn.LSTM(input_size + hidden_size, hidden_size, num_layers)
+        self.embedding = nn.Embedding(vocab_size, embedding_dim)
+        self.attention = Attention(hidden_size)
+        self.rnn = nn.LSTM(embedding_dim + hidden_size, hidden_size, num_layers=1, bidirectional=False)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x, encoder_states, hidden, cell):
-        context_vector, attention_weights = self.attention(hidden, encoder_states)
+        embedding = self.embedding(x)
+        context_vector, _ = self.attention(encoder_states, hidden)
         context_vector = context_vector.permute(1, 0, 2)
-        outputs, (hidden, cell) = self.rnn(torch.cat((x, context_vector), dim=2), (hidden,  cell))
-
-        pred = self.fc(outputs)
-        return pred, hidden, cell
+        outputs, (hidden, cell) = self.rnn(torch.cat((embedding, context_vector), dim=2), (hidden, cell))
+        outputs = self.fc(outputs)
+        return outputs, hidden, cell
